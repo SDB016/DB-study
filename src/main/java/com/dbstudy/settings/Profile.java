4b9ebce1a4095b0187950a7bf82de628acd20414
SDB016 <dongbin4013@naver.com>
2021-06-22 03:12:44 +0900

    Update Profile

--- src/main/java/com/dbstudy/settings/Profile.java
+++ src/main/java/com/dbstudy/settings/Profile.java
@@ -2,8 +2,10 @@ package com.dbstudy.settings;
 
 import com.dbstudy.domain.Account;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Profile {
 
     private String bio;
