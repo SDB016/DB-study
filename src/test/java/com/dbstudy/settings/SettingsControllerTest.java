e376dc6921f20904ef57c785abee7f50a0163043
SDB016 <dongbin4013@naver.com>
2021-06-23 17:36:28 +0900

    Update password Test

--- src/test/java/com/dbstudy/settings/SettingsControllerTest.java
+++ src/test/java/com/dbstudy/settings/SettingsControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -23,6 +24,7 @@ class SettingsControllerTest {
 
     @Autowired MockMvc mockMvc;
     @Autowired AccountRepository accountRepository;
+    @Autowired PasswordEncoder passwordEncoder;
 
     @AfterEach
     void afterEach() {
@@ -77,6 +79,51 @@ class SettingsControllerTest {
         Account dongbin = accountRepository.findByNickname("dongbin");
         assertNull(dongbin.getBio());
 
+    }
+
+    @WithAccount("dongbin")
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePassword_form() throws Exception{
+        mockMvc.perform(get(SettingsController.SETTINGS_PASSWORD_VIEW_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"));
+    }
+
+    @WithAccount("dongbin")
+    @DisplayName("패스워드 수정하기 - 입력값 정상")
+    @Test
+    void updatePassword_success() throws Exception{
+
+        String newPassword = "13131313";
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_VIEW_URL)
+                .param("newPassword", newPassword)
+                .param("newPasswordConfirm",newPassword)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PASSWORD_VIEW_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account byNickname = accountRepository.findByNickname("dongbin");
+        assertTrue(passwordEncoder.matches(newPassword,byNickname.getPassword()));
+    }
+
+    @WithAccount("dongbin")
+    @DisplayName("패스워드 수정하기 - 입력값 에러 - 패스워드 불일치")
+    @Test
+    void updatePassword_fail() throws Exception{
+
+        String newPassword = "13131313";
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_VIEW_URL)
+                .param("newPassword", newPassword)
+                .param("newPasswordConfirm",newPassword + "1111")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("passwordForm"))
+                .andExpect(model().attributeExists("account"));
     }
 
 }
