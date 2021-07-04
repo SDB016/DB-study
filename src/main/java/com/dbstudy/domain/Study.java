6170789e95913ca284c2382db1e3f24ff488e8d9
SDB016 <dongbin4013@naver.com>
2021-07-04 17:02:30 +0000

    Study domain

---
+++ src/main/java/com/dbstudy/domain/Study.java
@@ -0,0 +1,58 @@
+package com.dbstudy.domain;
+
+import lombok.*;
+import org.thymeleaf.standard.expression.Each;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @RequiredArgsConstructor @NoArgsConstructor
+public class Study {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers;
+
+    @ManyToMany
+    private Set<Account> members;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags;
+
+    @ManyToMany
+    private Set<Zone> zones;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private Boolean recruiting;
+
+    private Boolean published;
+
+    private Boolean closed;
+
+    private Boolean useBanner;
+
+
+}
