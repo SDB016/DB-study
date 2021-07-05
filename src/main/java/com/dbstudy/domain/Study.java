4dc4874b189ab64d84577e4ca2e831ae934a92bd
SDB016 <dongbin4013@naver.com>
2021-07-05 13:12:21 +0000

    Add Study

--- src/main/java/com/dbstudy/domain/Study.java
+++ src/main/java/com/dbstudy/domain/Study.java
@@ -5,21 +5,22 @@ import org.thymeleaf.standard.expression.Each;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
-@Builder @RequiredArgsConstructor @NoArgsConstructor
+@Builder @AllArgsConstructor @NoArgsConstructor
 public class Study {
 
     @Id @GeneratedValue
     private Long id;
 
     @ManyToMany
-    private Set<Account> managers;
+    private Set<Account> managers = new HashSet<>();
 
     @ManyToMany
-    private Set<Account> members;
+    private Set<Account> members = new HashSet<>();
 
     @Column(unique = true)
     private String path;
@@ -35,10 +36,10 @@ public class Study {
     private String image;
 
     @ManyToMany
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
 
     @ManyToMany
-    private Set<Zone> zones;
+    private Set<Zone> zones = new HashSet<>();
 
     private LocalDateTime publishedDateTime;
 
@@ -55,4 +56,7 @@ public class Study {
     private Boolean useBanner;
 
 
+    public void addManager(Account account) {
+        this.managers.add(account);
+    }
 }
---
+++ src/main/java/com/dbstudy/study/StudyRepository.java
@@ -0,0 +1,12 @@
+package com.dbstudy.study;
+
+import com.dbstudy.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    boolean existsByPath(String path);
+}
