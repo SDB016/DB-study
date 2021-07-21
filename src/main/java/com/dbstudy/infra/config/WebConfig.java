376be88e3a1a8175500cc6051b4b3abbf81b20a0
SDB016 <dongbin4013@naver.com>
2021-07-21 19:24:36 +0900

    Notification icon

---
+++ src/main/java/com/dbstudy/infra/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.dbstudy.infra.config;
+
+import com.dbstudy.modules.notification.NotificationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final NotificationInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> staticResourcePath = Arrays.stream(StaticResourceLocation.values())
+                .flatMap(StaticResourceLocation::getPatterns)
+                .collect(Collectors.toList());
+        staticResourcePath.add("/node_modules/**");
+
+        registry.addInterceptor(interceptor)
+        .excludePathPatterns(staticResourcePath);
+    }
+}
--- src/main/java/com/dbstudy/modules/notification/NotificationRepository.java
+++ src/main/java/com/dbstudy/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.dbstudy.modules.notification;
 
+import com.dbstudy.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
